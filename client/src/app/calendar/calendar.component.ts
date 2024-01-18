70263a2048930b688ecfca2e4c2f9d9ad598b7c7
Kabilan Nanthakumar <kabilannanthakumar@Kabilans-MBP.ht.home>
2024-01-18 13:17:52 -0500

    updated calendar logic

---
+++ client/src/app/calendar/calendar.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { DateBatch } from '../_models/dateBatch';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css'],
+})
+export class CalendarComponent implements OnInit {
+  selectedMonth: number = 0;
+  selectedYear: number = 2024;
+  dateRange: Date[] = [];
+  dateBatches: DateBatch[] = [];
+
+  ngOnInit(): void {
+    this.dateRange = this.initializeAppointmentRange();
+    this.dateBatches = this.initializeDateBatches(this.dateRange);
+
+    console.log(this.dateRange);
+    console.log(this.dateBatches);
+  }
+
+  private initializeAppointmentRange(): Date[] {
+    let dateToAdd: Date = new Date(this.selectedYear, this.selectedMonth, 1);
+    let monthCompleted: boolean = false;
+    let dateCounter: number = 0;
+    let dateRange: Date[] = [];
+
+    while (!monthCompleted) {
+      dateCounter++;
+
+      dateToAdd = new Date(this.selectedYear, this.selectedMonth, dateCounter);
+
+      if (dateToAdd.getMonth() !== this.selectedMonth) {
+        monthCompleted = true;
+      } else {
+        dateRange.push(dateToAdd);
+      }
+    }
+
+    return dateRange;
+  }
+
+  private initializeDateBatches(dateRange: Date[]): DateBatch[] {
+    let dateBatches: DateBatch[] = [];
+    let batchSplitComplete: boolean = false;
+    let dateRangeMutated: Date[] = dateRange.slice();
+
+    while (!batchSplitComplete) {
+      if (dateRangeMutated.length < 7) {
+        dateBatches.push(new DateBatch(dateRangeMutated));
+        batchSplitComplete = true;
+      } else {
+        dateBatches.push(new DateBatch(dateRangeMutated.splice(0, 7)));
+      }
+    }
+
+    return dateBatches;
+  }
+}
