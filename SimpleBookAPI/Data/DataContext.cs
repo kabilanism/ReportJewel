015d120b2de2377d27cefbd237ac831928bb9b35
Kabilan Nanthakumar <kabilannanthakumar@Kabilans-MBP.ht.home>
2024-01-24 09:56:52 -0500

    added db context layer and initial entity

---
+++ SimpleBookAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleBookAPI.Entities;
+
+namespace SimpleBookAPI.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<FormControl> Event { get; set; }
+  }
+}
+
+
---
+++ SimpleBookAPI/Entities/FormControl.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleBookAPI.Entities
+{
+  public class FormControl
+  {
+    public int FormControlId { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Placeholder { get; set; }
+    public string Label { get; set; }
+    public int Order { get; set; }
+    public string Value { get; set; }
+    public List<SelectOption> Options { get; set; } = new();
+  }
+
+  public class SelectOption
+  {
+    public string Value { get; set; }
+    public string DisplayValue { get; set; }
+  }
+}
+
