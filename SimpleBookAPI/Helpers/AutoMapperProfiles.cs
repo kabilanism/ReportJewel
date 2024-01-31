f24f4f4bdd4c83bd69e8a668a243c35a95d9a217
Kabilan Nanthakumar <kabilannanthakumar@Kabilans-MBP.ht.home>
2024-01-31 16:11:04 -0500

    added automapper profiles

---
+++ SimpleBookAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SimpleBookAPI.Data.DTOs;
+using SimpleBookAPI.Entities;
+
+namespace SimpleBookAPI.Helpers
+{
+  public class AutoMapperProfiles : Profile
+  {
+    public AutoMapperProfiles()
+    {
+      CreateMap<User, UserDto>();
+      CreateMap<Form, FormDto>();
+      CreateMap<Client, ClientDto>();
+    }
+  }
+}
+
