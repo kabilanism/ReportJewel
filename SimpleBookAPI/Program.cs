34f789ba2706e59cf6fd1310499320591e5a50a7
Kabilan Nanthakumar <kabilannanthakumar@Kabilans-MBP.ht.home>
2024-01-31 16:13:11 -0500

    update start up and dependencies

--- SimpleBookAPI/Program.cs
+++ SimpleBookAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SimpleBookAPI.Data;
+using SimpleBookAPI.Data.Repositories;
+using SimpleBookAPI.Data.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +13,18 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
