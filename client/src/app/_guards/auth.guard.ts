03b9bf453c686177a2cf861418d835ae97f08c87
Kabilan Nanthakumar <kabilannanthakumar@Kabilans-MBP.ht.home>
2024-02-01 13:08:11 -0500

    added auth guard, updated route config

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { map } from 'rxjs';
+import { UserService } from '../_services/user.service';
+import { ToastrService } from 'ngx-toastr';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const userService = inject(UserService);
+  const toastr = inject(ToastrService);
+
+  return userService.currentUser$.pipe(
+    map((user) => {
+      if (user) {
+        return true;
+      } else {
+        toastr.error('You shall not pass!');
+        return false;
+      }
+    })
+  );
+};
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { authGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
