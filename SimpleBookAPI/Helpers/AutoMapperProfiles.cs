c17ec4dd3ca76faee3b16f2453869711091c5fa5
Kabilan Nanthakumar <kabilannanthakumar@Kabilans-MBP.ht.home>
2024-02-04 08:29:06 -0500

    added mapper profile for form control

--- SimpleBookAPI/Helpers/AutoMapperProfiles.cs
+++ SimpleBookAPI/Helpers/AutoMapperProfiles.cs
@@ -9,7 +9,9 @@ namespace SimpleBookAPI.Helpers
     public AutoMapperProfiles()
     {
       CreateMap<User, UserDto>();
-      CreateMap<Form, FormDto>();
+      CreateMap<Form, FormDto>()
+        .ForMember(dest => dest.Controls, opt => opt.MapFrom(src => src.FormControls));
+      CreateMap<FormControl, FormControlDto>();
       CreateMap<Client, ClientDto>();
     }
   }
