afacb4feebc243f5f827885b725a58a15830007e
Kabilan Nanthakumar <kabi.nanthakumar@gmail.com>
2024-02-16 15:41:04 -0500

    add register method to user service

--- client/src/app/_services/user.service.ts
+++ client/src/app/_services/user.service.ts
@@ -3,6 +3,7 @@ import { User } from '../_models/user';
 import { BehaviorSubject, Observable, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { RegisterUser } from '../_models/registerUser';
 
 @Injectable({
   providedIn: 'root',
@@ -15,12 +16,21 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
+  register(registerUser: RegisterUser): Observable<User> {
+    return this.http
+      .post<User>(`${this.baseUrl}user/register`, registerUser)
+      .pipe(
+        map((user: User) => {
+          this.setCurrentUser(user);
+          return user;
+        })
+      );
+  }
+
   login(model: any): Observable<void> {
     return this.http.post<User>(`${this.baseUrl}user/login`, model).pipe(
       map((user: User) => {
-        if (user) {
-          this.setCurrentUser(user);
-        }
+        this.setCurrentUser(user);
       })
     );
   }
