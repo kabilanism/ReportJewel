0f0f31a14cb0f8497279fffd91f60245c5ffb391
Kabilan Nanthakumar <kabi.nanthakumar@gmail.com>
2024-02-24 09:09:57 -0500

    add fallback controller to fix routing issue

--- ReportJewelAPI/Program.cs
+++ ReportJewelAPI/Program.cs
@@ -24,6 +24,7 @@ app.UseAuthorization();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
