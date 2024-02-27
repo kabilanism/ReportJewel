8b6b7e4ac1979d44e5f24f18f73d7d3134506706
Kabilan Nanthakumar <kabi.nanthakumar@gmail.com>
2024-02-27 00:30:57 -0500

    update app service extensions

--- ReportJewelAPI/Extensions/ApplicationServiceExtensions.cs
+++ ReportJewelAPI/Extensions/ApplicationServiceExtensions.cs
@@ -11,11 +11,6 @@ namespace ReportJewelAPI.Extensions
   {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
-      });
-
       services.AddCors();
       services.AddScoped<IUserRepository, UserRepository>();
       services.AddScoped<ILayoutRepository, LayoutRepository>();
